8f9a3d6a2cc6c551075c4bcc1895ea49f370a438
Juan Antonio Pavón Carmona <44985444+japc78@users.noreply.github.com>
2022-05-07 20:19:46 +0200

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/japc78/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('japc78')
+            node / gitConfigEmail('japc.grafico@gmail.com')
         }
     }
     steps {
